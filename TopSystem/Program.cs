3d27c917992b8e6810e7bfae99080db7be0f60c6
Артём <@KOMPUTER>
2024-04-10 15:58:54 +0300

    Добавьте файлы проекта.

---
+++ TopSystem/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSystem
+{
+    public abstract class Shape
+    {
+        public abstract void Draw();
+    }
+    public  class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Circle");
+          
+        }
+    }
+    public class Ellipse : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Ellipse") ;
+        }
+    }
+
+    public class Triangle : Shape 
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Triangle");
+        }
+    }
+
+    public class Square : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Square");
+        }
+    }
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[]
+            {
+                new Circle(),
+                new Ellipse(),
+                new Triangle(),
+                new Square()
+            };
+
+            foreach (Shape shape in shapes) 
+            {
+                shape.Draw();
+            }
+
+        }
+    }
+}
